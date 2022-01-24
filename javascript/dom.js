2db3d9a68e5568dc7b4c731a0d42f113458f1240
roxyzc <roxyzc12@gmail.com>
2022-01-24 20:30:24 +0700

    menambahkan querySelector dan querySelectorAll pada dom.js

--- javascript/dom.js
+++ javascript/dom.js
@@ -21,3 +21,18 @@ h1.style.fontSize = "3rem";
 // -> HTMLCollection
 let p1 = document.getElementsByClassName('p1')[0];
 p1.innerHTML = 'ini diubah dari javascript';
+
+// document.querySelector()
+// -> element
+let selectorB = document.querySelector('#b p');
+selectorB.style.color = 'green';
+
+let li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.color = 'orange';
+
+// document.querySelectorAll()
+// -> Node list
+let pAll = document.querySelectorAll('p');
+for(let i = 0; i < pAll.length; i++){
+    pAll[i].style.fontSize = "2.5rem";
+}
